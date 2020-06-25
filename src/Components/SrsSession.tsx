7a41f175af86ca87aac55f1a8646dd1f57df1faf
Kevin Cipolla <k2cipolla@gmail.com>
2020-06-25 09:14:33 -0400

    started SrsSession

--- src/Components/SrsSession.tsx
+++ src/Components/SrsSession.tsx
@@ -4,10 +4,10 @@ import React, { useReducer } from 'react';
 
 const initialState = {srsPhase: 'QUESTION_PHASE'};
 
-const srsSessionReducer = (state, action) {
+const srsSessionReducer = (state, action) => {
   switch (action.type) {
     case 'SHOW_BACK':
-      
+
 
   }
 }
