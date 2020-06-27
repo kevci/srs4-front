a93d72fb65efc0e67e0539154119a30cd5a8dafb
Kevin Cipolla <k2cipolla@gmail.com>
2020-06-27 11:50:15 -0400

    fixed indentation in Login and SignUp

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {Link, RouteComponentProps } from '@reach/router';
 
 const Login: React.FC<RouteComponentProps> = () => {
-    return (
-      <>
-      </>
-    );
-  }
+  return (
+    <>
+    </>
+  );
+}
   
   export default Login;
--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {Link, RouteComponentProps } from '@reach/router';
 
 const SignUp: React.FC<RouteComponentProps> = () => {
-    return (
-      <>
-      </>
-    );
-  }
+  return (
+    <>
+    </>
+  );
+}
   
   export default SignUp;
